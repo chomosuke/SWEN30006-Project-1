8e50923f0e0b7f2dd6549f8e432ceccfa5ef6533
chomosuke <38484873+chomosuke@users.noreply.github.com>
2020-09-28 15:22:08 +1300

    i forgot to do FoodItem

--- Automail/src/automail/FoodItem.java
+++ Automail/src/automail/FoodItem.java
@@ -4,4 +4,9 @@ public class FoodItem extends MailItem {
 	public FoodItem(int dest_floor, int arrival_time, int weight) {
 		super(dest_floor, arrival_time, weight);
 	}
+	
+	@Override
+	public String toString() {
+		return String.format("Food Item:: ID: %6s | Arrival: %4d | Destination: %2d | Weight: %4d", id, arrival_time, destination_floor, weight);
+	}
 }
